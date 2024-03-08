e0a430b3b59b6c20cac0c1ea8e0a9f4e8bc897f0
heizhan <humbertorbw@gmail.com>
2024-03-08 11:40:08 -0400

    se creo la clase activity con sus propiedades

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,19 @@ document.getElementsByTagName("form")[0].addEventListener("submit", (event) => {
     event.preventDefault();
 });
 
-const Activity = [];
+class Activity {
+    constructor(id, title, description, imgUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+}
+
+const actividades = [];
 const noActivity = `<h2>¡No hay Actividad!</h2>`;
 const activityBox = document.getElementById("containerActivity");
 
-if (Activity.length === 0) activityBox.innerHTML = noActivity;
+if (actividades.length === 0) activityBox.innerHTML = noActivity;
 
 console.log(activityBox);
